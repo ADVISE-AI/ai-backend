e3c0949b0494189ea38da1da6ed80ed3f1cd6d65
Darkus <kdayushbornin2004@gmail.com>
2025-09-14 12:23:52 +0530

    something

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
 import os
 from glob import glob
 from config import DB_URL, logger
-from db import pool
 from whatsapp import upload_video
-
+from config import conn
 _logger = logger(__name__)
 
 
@@ -80,54 +79,10 @@ def refactor_dict(data: dict) -> dict:
 
     return {"Error": "unhandled payload", "raw": value}
 
-def db_saver():
-    files = [
-    glob("/home/darkus/Desktop/whatsapp-ai-bot/media/Anniversary/*"),
-    glob("/home/darkus/Desktop/whatsapp-ai-bot/media/Birthday/*"),
-    glob("/home/darkus/Desktop/whatsapp-ai-bot/media/wedding/2D Sample/*"),
-    glob("/home/darkus/Desktop/whatsapp-ai-bot/media/wedding/2D Sample with Caricature/*"),
-    glob("/home/darkus/Desktop/whatsapp-ai-bot/media/wedding/3D Sample with Caricature/*"),
-    ]
-
-    for file_group in files:
-        for f in file_group:
-            try:
-                media_id = upload_video(f)
-                media_name = os.path.basename(f)
-
-                # detect type
-                media_type = next((t for t in ["anniversary", "birthday", "wedding"] if t in f.lower()), "")
-
-                # detect description
-                desc = next(
-                    (d for d in [
-                        "2d sample",
-                        "2d sample with caricature",
-                        "3d sample with caricature"
-                    ] if d in f.lower()),
-                    "2d with caricature"
-                )
-
-                print(f"Name: {media_name}, ID: {media_id}, Type: {media_type}, Description: {desc}")
-
-                with pool.connection() as conn:
-                    conn.autocommit=True
-                    with conn.cursor() as cur:
-                        cur.execute("""
-                            INSERT INTO sample_media_library (media_name, media_id, media_type, media_description)
-                            VALUES (%s, %s, %s, %s);
-                        """, (media_name.lower(), str(media_id), media_type.lower(), desc.lower()))
-                    conn.commit()
-
-            except Exception as e:
-                _logger.error("Error processing file %s: %s", f, e)
-
-    print("Operation Done!")
- 
 
 def search_db_tool(media_type:str, media_description: str) -> list:
     id_list = []
-    with pool.connection() as conn:
+    with conn:
         with conn.cursor() as curr:
             sql_query = """SELECT media_id FROM sample_media_library WHERE media_type = %s AND media_description = %s"""
             curr.execute(sql_query, (media_type.lower(), media_description.lower()))
